5d29844860d99a44a1d10113b678a98d5089e79d
thecampbells384 <thecampbells384@gmail.com>
2022-03-04 17:41:04 -0800

    lookup_treatment and treatments_from_plot_id were updated to return site and plot information as well as treatment data.

--- R/lookup_treatment.R
+++ R/lookup_treatment.R
@@ -1,7 +1,7 @@
 #' Look up treatments given plot ID
-#' 
+#'
 #' A function which accepts a plot_id and returns a named list providing the OTC,
-#' Snow, and Fertaliser treatments. The data is looked up in the tundra::plot_names 
+#' Snow, and Fertaliser treatments. The data is looked up in the tundra::plot_names
 #' dataframe. If the plot_id does not return one, and only one hit, then NA is returned.
 #' @param id The plot Id to be looked up to find the corresponding treatment data
 #' @param plot_df The dataframe which will be searched for the treatment data. Defaults
@@ -10,23 +10,27 @@
 
 
 lookup_treatment <- function(id, plot_df = tundra::plot_names){
-  
+
   plot_data <- dplyr::filter(plot_df, plot_id == id)
-  
+
   if (nrow(plot_data) != 1){
     treatment_data <- list(
       otc = NA,
       snow = NA,
-      fert = NA
+      fert = NA,
+      site = NA,
+      plot = NA
     )
   } else {
-    
+
     treatment_data <- list(
       otc = plot_data$otc_treatment,
       snow = plot_data$snow_treatment,
-      fert = plot_data$fert_treatment
+      fert = plot_data$fert_treatment,
+      site = plot_data$site,
+      plot = plot_data$plot
     )
-    
+
   }
   treatment_data
 }
--- R/treatments_from_plot_id.R
+++ R/treatments_from_plot_id.R
@@ -1,11 +1,11 @@
 #' Clean treatment columns by looking up plot IDs
-#' 
+#'
 #' A function which accepts a dataframe with plot IDs and returns a dataframe with all
-#' treatment columns (OTC, snow and fert) completed based on the treatment information
-#' available for that plot in tundra::plot_names. If the plot ID can not be identified then
-#' the treatment info is unchanged. The input dataframe must be in the format produced by
-#' the standardise_plots function, in that it must have columns named "plot_id", "otc_treatment",
-#' "snow_treatment", and "fert_treatment".
+#' treatment columns (OTC, snow and fert), as well as the site and plot data completed based on
+#' the information available for that plot in tundra::plot_names. If the plot ID can not be
+#' identified then the treatment info is unchanged. The input dataframe must be in the format
+#' produced by the standardise_plots function, in that it must have columns named "plot_id",
+#' "otc_treatment", "snow_treatment", "fert_treatment", "site", and "plot".
 #' @param dataframe The dataframe that is to have its treatment data completed based on
 #' the plot_ids it already contains and the corresponding treatment info for those plots found
 #' in tundra::plot_names
@@ -13,36 +13,44 @@
 
 
 treatments_from_plot_id <- function(dataframe){
-  # Add row_number to dataframe to line up the results of lookup_treatment with the data
-  dataframe <- dplyr::mutate(dataframe, .row = 1:nrow(dataframe))
-  # Produce a dataframe containing the treatment info, and the corresponding row number in dataframe
-  dataframe <- dplyr::group_by(dataframe, plot_id)
-  treatments <- dplyr::group_modify(dataframe, ~ {data.frame(.row = .x$.row,
-                                                             .lookup_otc = rep(lookup_treatment(.y$plot_id)$otc,
-                                                                              nrow(.x)),
-                                                             .lookup_snow = rep(lookup_treatment(.y$plot_id)$snow,
-                                                                               nrow(.x)),
-                                                             .lookup_fert = rep(lookup_treatment(.y$plot_id)$fert,
-                                                                               nrow(.x))
-  )
-  })
-  # Merge the dataframe with the treatments
-  dataframe <- merge(dataframe, treatments)
-  
-  dataframe <- dplyr::ungroup(dataframe)
-  
-  # Update the treatment information where the lookup_treatment function returned non NA data
-  dataframe <- dplyr::mutate(dataframe, otc_treatment = ifelse(is.na(.lookup_otc),
-                                                               otc_treatment, .lookup_otc),
-                             snow_treatment = ifelse(is.na(.lookup_snow),
-                                                    snow_treatment, .lookup_snow),
-                             fert_treatment = ifelse(is.na(.lookup_fert),
-                                                    fert_treatment, .lookup_fert)
-  )
-  # return dataframe to original sorting
-  dataframe <- dplyr::arrange(dataframe, .row)
-  # remove the .row column added as a key for the asigning of the treatments as well as the
-  # lookup_treatment columns
-  dataframe <- dplyr::select(dataframe, !c(.row, .lookup_otc, .lookup_snow, .lookup_fert))
-  dataframe
+    # Add row_number to dataframe to line up the results of lookup_treatment with the data
+    dataframe <- dplyr::mutate(dataframe, .row = 1:nrow(dataframe))
+    # Produce a dataframe containing the treatment info, and the corresponding row number in dataframe
+    dataframe <- dplyr::group_by(dataframe, plot_id)
+    treatments <- dplyr::group_modify(dataframe, ~ {data.frame(.row = .x$.row,
+                                                               .lookup_otc = rep(lookup_treatment(.y$plot_id)$otc,
+                                                                                 nrow(.x)),
+                                                               .lookup_snow = rep(lookup_treatment(.y$plot_id)$snow,
+                                                                                  nrow(.x)),
+                                                               .lookup_fert = rep(lookup_treatment(.y$plot_id)$fert,
+                                                                                  nrow(.x)),
+                                                               .lookup_site = rep(lookup_treatment(.y$plot_id)$site,
+                                                                                  nrow(.x)),
+                                                               .lookup_plot = rep(lookup_treatment(.y$plot_id)$plot,
+                                                                                  nrow(.x))
+    )
+    })
+    # Merge the dataframe with the treatments
+    dataframe <- merge(dataframe, treatments)
+
+    dataframe <- dplyr::ungroup(dataframe)
+
+    # Update the treatment information where the lookup_treatment function returned non NA data
+    dataframe <- dplyr::mutate(dataframe, otc_treatment = ifelse(is.na(.lookup_otc),
+                                                                 otc_treatment, .lookup_otc),
+                               snow_treatment = ifelse(is.na(.lookup_snow),
+                                                       snow_treatment, .lookup_snow),
+                               fert_treatment = ifelse(is.na(.lookup_fert),
+                                                       fert_treatment, .lookup_fert),
+                               site = ifelse(is.na(.lookup_site),
+                                                       site, .lookup_site),
+                               plot = ifelse(is.na(.lookup_plot),
+                                                       plot, .lookup_plot)
+    )
+    # return dataframe to original sorting
+    dataframe <- dplyr::arrange(dataframe, .row)
+    # remove the .row column added as a key for the asigning of the treatments as well as the
+    # lookup_treatment columns
+    dataframe <- dplyr::select(dataframe, !c(.row, .lookup_otc, .lookup_snow, .lookup_fert, .lookup_site, .lookup_plot))
+    dataframe
 }
